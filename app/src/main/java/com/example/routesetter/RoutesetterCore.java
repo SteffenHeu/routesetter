6765e0189a0c333fe620b0be4cca625d8836e4fb
SteffenHeu <steffen.heuckeroth@gmx.de>
2020-10-30 17:34:58 +0100

    added basic action menu

--- app/src/main/java/com/example/routesetter/RoutesetterCore.java
+++ app/src/main/java/com/example/routesetter/RoutesetterCore.java
@@ -1,20 +1,40 @@
 package com.example.routesetter;
 
 import android.os.Bundle;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.appcompat.widget.Toolbar;
 
 public class RoutesetterCore extends AppCompatActivity {
+
+  public static final String TAG = "RoutesetterCore";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.layout);
 
-
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
   }
 
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.miNewRoute:
+        Log.i(TAG, "add route clicked");
+        // TODO create new route
+        break;
+    }
+    return true;
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.action_menu, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
 }
